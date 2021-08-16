1636a5b13787b1a38db2c73ee5267ec7fe1a4bc2
Anton Churkin <anton.churkin@ua.akvelon.com>
2021-08-16 19:05:15 +0300

    Added serializer for UPDATE; fixed required fields.

--- apis/serializers.py
+++ apis/serializers.py
@@ -3,6 +3,25 @@ from taskTracks import models
 
 
 class TaskTrackSerializer(serializers.ModelSerializer):
+    date = serializers.CharField(required=True)
+    state = serializers.CharField(required=True)
+    priority = serializers.CharField(required=True)
     class Meta:
         model = models.TaskTrack
         fields = '__all__'
+
+    def get_validation_exclusions(self):
+        exclusions = super(TaskTrackSerializer, self).get_validation_exclusions()
+        return exclusions + ['date','state','priority']
+
+
+class UpdateTaskSerializer(serializers.ModelSerializer):
+    state = serializers.CharField(required=True)
+    class Meta:
+        model = models.TaskTrack
+        fields = ['description', 'state']
+
+    def get_validation_exclusions(self):
+        exclusions = super(UpdateTaskSerializer, self).get_validation_exclusions()
+        return exclusions + ['state']
+
--- apis/views.py
+++ apis/views.py
@@ -1,12 +1,23 @@
+import datetime
+from django.utils import timezone
 from django.shortcuts import render
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, generics
 from taskTracks import models
-from .serializers import TaskTrackSerializer
+from .serializers import TaskTrackSerializer, UpdateTaskSerializer
 
 
 class TaskTrackViewSet(viewsets.ModelViewSet):
     queryset = models.TaskTrack.objects.all()
     serializer_class = TaskTrackSerializer
+    serializer_action_classes = {
+        'update': UpdateTaskSerializer,
+    }
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['id','priority','state','date']
-    
+
+
+    def get_serializer_class(self):
+       try:
+           return self.serializer_action_classes[self.action]
+       except (KeyError, AttributeError):
+           return super().get_serializer_class()
