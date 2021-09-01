258d6b38c37486d5b2b916d2fdcadca106a6b0eb
Anton Churkin <anton.churkin@ua.akvelon.com>
2021-09-01 18:06:34 +0300

    Cleared code

--- apis/apps.py
+++ apis/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_init, post_save, post_delete
-from .tasks import notify_task_created, notify_task_updated, notify_task_deleted
 
 
 class ApisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apis'
-
-    # def ready(self):
-    #     post_init.connect(notify_task_created)
-    #     post_save.connect(notify_task_updated)
-    #     post_delete.connect(notify_task_deleted)
