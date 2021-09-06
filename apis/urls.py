f8a42c2a223c2e1a696671a6dac8159a287947a4
Anton Churkin <anton.churkin@ua.akvelon.com>
2021-09-06 16:47:15 +0300

    Updated URLs

--- apis/urls.py
+++ apis/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 from rest_framework.routers import DefaultRouter
 
-
+app_name = 'apis'
 router = DefaultRouter()
 router.register('', views.TaskTrackViewSet, basename='tasktracks')
 
