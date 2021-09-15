92b725c152bb1c0c87ccd90aa093dbc8a3cf2381
Anton Churkin <anton.churkin@ua.akvelon.com>
2021-09-15 16:15:59 +0300

    Refactored serializers

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,71 +1,41 @@
 from rest_framework import serializers
-from taskTracks import models
 from rest_framework.exceptions import ValidationError
+import threading
 import pika
+import os
 
-
-#  BROKEN - doesnt work with Celery Scheduler
-def emit_notification(message):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.exchange_declare(exchange='logs', exchange_type='fanout')
-    channel.basic_publish(
-        exchange='logs',
-        routing_key='',
-        body=message
-    )
-    print(" [x] Sent %r" % message)
-    connection.close()
+from taskTracks.models import TaskTrack
 
 
 class ListTaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.TaskTrack
+        model = TaskTrack
         fields = '__all__'
-
-
-class DeleteTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.TaskTrack
-        fields = '__all__'
-        # emit_notification("Task deleted!")
 
 
 class UpdateTaskSerializer(serializers.ModelSerializer):
-    state = serializers.ChoiceField(choices=models.TaskTrack.States, required=True)
-
     class Meta:
-        model = models.TaskTrack
+        model = TaskTrack
         fields = ['description', 'state']
 
-    def is_valid(self, raise_exception=False):
-        valid = super(UpdateTaskSerializer, self).is_valid()
-        if valid:
-            # emit_notification("Task updated!")
-            return True
-        else:
-            if self._errors:
-                if raise_exception:
-                    raise ValidationError(self.errors)
-            return False
 
+"""Obsolete - will be removed later"""
+# class CreateTaskSerializer(serializers.ModelSerializer):
+#     name = serializers.CharField(required=True)
+#     description = serializers.CharField(required=True)
+#     # date = serializers.DateTimeField()
+#     state = serializers.ChoiceField(choices=models.TaskTrack.States, required=True)
+#     priority = serializers.ChoiceField(choices=models.TaskTrack.Priorities, required=True)
 
-class CreateTaskSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(required=True)
-    description = serializers.CharField(required=True)
-    # date = serializers.DateTimeField()
-    state = serializers.ChoiceField(choices=models.TaskTrack.States, required=True)
-    priority = serializers.ChoiceField(choices=models.TaskTrack.Priorities, required=True)
+#     class Meta:
+#         model = models.TaskTrack
+#         fields = '__all__'
+#         read_only_fields = ['id']
 
-    class Meta:
-        model = models.TaskTrack
-        fields = '__all__'
-        read_only_fields = ['id']
-
-    def is_valid(self, raise_exception=False):
-        valid = super(CreateTaskSerializer, self).is_valid()
-        if valid:
-            # emit_notification("Task created!")
-            return True
-        else:
-            return False
+#     def is_valid(self, raise_exception=False):
+#         valid = super(CreateTaskSerializer, self).is_valid()
+#         if valid:
+#             # emit_notification("Task created!")
+#             return True
+#         else:
+#             return False
