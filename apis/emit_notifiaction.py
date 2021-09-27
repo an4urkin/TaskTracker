8e47627007d31ec75c25996e8c21856dcc1383aa
Anton Churkin <anton.churkin@ua.akvelon.com>
2021-09-27 19:37:55 +0300

    Refactored notifications

---
+++ apis/emit_notifiaction.py
@@ -0,0 +1,37 @@
+import pika, time
+from datetime import datetime
+
+
+def emit_notification(message):
+
+    cred = pika.PlainCredentials('guest', 'guest')
+    params = pika.ConnectionParameters(
+        host='rabbitmq', # for local -> 'localhost' || for docker -> 'rabbitmq'
+        port=5672,
+        virtual_host='/',
+        credentials=cred
+    )
+    
+    try:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.exchange_declare(exchange='logs', exchange_type='fanout')
+        channel.basic_publish(
+            exchange='logs',
+            routing_key='',
+            body=message
+        )
+        print(' [x] Sent %r' % message)
+        connection.close()
+        return(message)
+    except:
+        print ('[',datetime.now(),'] Could not connect to amqp: Connection refused.\nMessage not sent.')
+        return('Message not sent.')
+
+        # for i in range(3):
+        #     if (i==2):
+        # 
+        #     else:
+        #         print ('[',datetime.now(),'] amqp connection error.\nTrying again...')
+        #         # avoid rapid reconnection on longer RMQ server outage
+        #         time.sleep(0.5)
