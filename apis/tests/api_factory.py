f66d79907d10cf30e4d212d3ed50072987366b1e
Anton Churkin <anton.churkin@ua.akvelon.com>
2021-10-28 17:43:52 +0300

    Cleared code

--- apis/tests/api_factory.py
+++ apis/tests/api_factory.py
@@ -1,9 +1,21 @@
 import factory
 import random
 from django.utils import timezone
-from taskTracks.models import TaskTrack
+from taskTracks.models import TaskTrack, User
 
 factory.Faker._DEFAULT_LOCALE = 'en_US'
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    
+    username = factory.Faker('name')
+    email = factory.Faker('free_email')
+    password = factory.Faker('bothify', text='????####')
+    is_staff =  factory.Faker('pybool')
+    is_active = factory.Faker('pybool')
+
 
 class TaskTrackFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -14,3 +26,5 @@ class TaskTrackFactory(factory.django.DjangoModelFactory):
     date = factory.LazyFunction(timezone.now)
     state = random.choice(TaskTrack.States.choices)
     priority = random.choice(TaskTrack.Priorities.choices)
+    owner = factory.Iterator(User.objects.all())
+
